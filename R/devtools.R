ced53912b1699ad4ddf2d77146f6ff0ea506e18d
sfeuerriegel <stefan.feuerriegel@is.uni-freiburg.de>
2016-08-28 11:08:25 +0200

    Added flag for updating README.Rmd in remakePackage()

--- R/devtools.R
+++ R/devtools.R
@@ -2,13 +2,18 @@
 #'
 #' Simple wrapper to rebuild, load and check package. This function specifically
 #' updates the manual as well.
+#' @param  updateReadme Flag whether to update the README.Rmd (default: FALSE).
 #' @examples
 #' \dontrun{
 #'   remakePackage()
 #' }
 #' @export
-remakePackage <- function() {
+remakePackage <- function(updateReadme = FALSE) {
   devtools::build()
   devtools::load_all()
   devtools::check()
+
+  if (updateReadme) {
+    system("Rscript -e \"rmarkdown::render('README.Rmd')\"")
+  }
 }
